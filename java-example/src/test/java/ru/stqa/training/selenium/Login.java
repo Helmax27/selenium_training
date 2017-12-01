779455a9774f0116682c99fd56a4637a005f346b
Helmax27 <olena.tsaturian@gmail.com>
2017-12-01 21:24:09 +0200

    Выполнен сценарий логина

---
+++ java-example/src/test/java/ru/stqa/training/selenium/Login.java
@@ -0,0 +1,18 @@
+package ru.stqa.training.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class Login extends TestBase {
+
+  @Test
+  public void myFirstTest() {
+    driver.navigate().to("http://localhost/litecart/admin/");
+    driver.findElement(By.name("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.name("login")).click();
+    wait.until(titleIs("My Store"));
+  }
+}
---
+++ java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -0,0 +1,16 @@
+package ru.stqa.training.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest extends TestBase {
+  @Test
+  public void myFirstTest() {
+    driver.navigate().to("https://www.google.com");
+    driver.findElement(By.name("q")).sendKeys("webdriver");
+    driver.findElement(By.name("btnK")).click();
+    wait.until(titleIs("webdriver - Поиск в Google"));
+  }
+}
