82bdfde485be0661f00f7f6668998c2f59cdf570
Helmax27 <olena.tsaturian@gmail.com>
2017-12-04 21:38:55 +0200

    Выполнен запуск в IE

--- java-example/src/test/java/ru/stqa/training/selenium/TestBrowsers.java
+++ java-example/src/test/java/ru/stqa/training/selenium/TestBrowsers.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
@@ -19,7 +21,7 @@ public class TestBrowsers {
 
   @Before
   public void start() {
-    driver = new ChromeDriver();
+    driver = new InternetExplorerDriver();
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     wait = new WebDriverWait(driver, 10);
   }
